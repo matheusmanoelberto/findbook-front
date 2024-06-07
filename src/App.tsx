70fd8ff6cc154df46774c90265b02c9d6b68ae2b
matheusmanoelberto <matheusmanoel51@gmail.com>
2024-06-07 18:23:33 -0300

    start title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header/Header";
+import { Title } from "./components/Title/Title";
 
 function App() {
   return (
     <>
       <Header />
+      <Title title="Test" />
     </>
   );
 }
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,7 @@
+type Props = {
+  title: string;
+};
+
+export function Title({ title }: Props) {
+  return <div>{title}</div>;
+}
